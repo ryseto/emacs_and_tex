a713b684226026051440046b16616c53078bce66
Ryohei Seto <setoryohei@me.com>
2012-02-26 11:24:01 +0100

    The configurations are set by reading rc files.

--- semiautotex.sh
+++ semiautotex.sh
@@ -1,52 +1,66 @@
 #########################################################################
-#### SemiAutoTeX 
-#### Time-stamp: <2012-02-25 05:51:49 seto>
+#### SemiAutoTeX ver 0.01
+#### Time-stamp: <2012-02-26 11:15:46 seto>
 #########################################################################
-## You can find some explanations at
-## http://d.hatena.ne.jp/setoryohei/20120219
+# You can find some explanations at
+# http://d.hatena.ne.jp/setoryohei/20120219
+#
+# The rc file '.semiautotexrc' should be put at your home directory.
+# Example of '~/.semiautotexrc'
+# latex="pdflatex -synctex=1"
+# latexdraft="pdflatex -draftmode"
+# bibtex="bibtex"
+# makeindex="makeindex"
+#
 #########################################################################
 #!/bin/sh
 if [ $# == 0 -o "$1" == "-h" -o "$1" == "-help"  ]; then
-    echo "Usage: semiautotex [-b] [-i] [-pd] TEXFILE 
+    echo "SemiAutoTeX 0.01:  Semi-automatic LaTeX document generation routine
+Usage: semiautotex [-b] [-i] TEXFILE 
 Options:
 -b          run BibTeX with LaTeX
--i          run MakeIndex with LaTeX
--pd         generate pdf by dvipdf
--pl         use platex"
+-i          run MakeIndex with LaTeX"
     exit 0
 fi
 
 mode="tex"
-pdfdvi=0
-use_platex=0
-
 while [ "${1:0:1}" == "-" ]; do
     if [ "$1" == "-b" ]; then
 	mode="bib"
     elif [ "$1" == "-i" ]; then
 	mode="idx"
-    elif [ "$1" == "-pd" ]; then
-	pdfdvi=1
-    elif [ "$1" == "-pl" ]; then
-	use_platex=1
     fi
     shift
 done
 
-if [ $use_platex = 1 ]; then
-## For platex
+
+if [ -f ${HOME}/.semiautotexrc ]; then
+    rcfile="${HOME}/.semiautotexrc"
+    DVIPDF=""
+    while read LINE
+    do
+	if [ "${LINE:0:1}" != "#" ]; then
+	    field=`echo ${LINE} | cut -d"=" -f1`
+	    case "$field" in
+		"latex" ) 
+		    LATEX="`echo ${LINE} | cut -d'\"' -f2`";;
+		"latexdraft" )
+		    LATEXDRAFT="`echo ${LINE} | cut -d'\"' -f2`";;
+		"bibtex" )
+		    BIBTEX="`echo ${LINE} | cut -d'\"' -f2`";;
+		"makeindex" )
+		    MAKEINDEX="`echo ${LINE} | cut -d'\"' -f2`";;
+		"dvipdf" )
+		    DVIPDF="`echo ${LINE} | cut -d'\"' -f2`";;
+	    esac
+	fi
+    done <$rcfile
+else
     LATEX="platex"
     LATEXDRAFT="platex"
     BIBTEX="pbibtex"
     MAKEINDEX="mendex -U"
     DVIPDF="dvipdfmx"
-else
-## For PDFLaTeX
-    LATEX="pdflatex -synctex=1"
-    LATEXDRAFT="pdflatex -draftmode"
-    BIBTEX="bibtex"
-    MAKEINDEX="makeindex"
-    DVIPDF=""
 fi
 
 texfile=`basename $1 .tex`
@@ -68,21 +82,19 @@ case "$mode" in
 		$LATEX $texfile 
 		message=`echo "$message + typeset"`
 	    done
-	fi
+	fi 
 	;;
     "bib" )
 	$LATEXDRAFT $texfile && typset_pass=1
 	if [ $typset_pass = 1 ]; then
 	    $BIBTEX $texfile 
-	    $LATEXDRAFT $texfile \
-		&& $LATEX $texfile
+	    $LATEXDRAFT $texfile && $LATEX $texfile
 	    message="typeset + BibTeX + typeset + typeset"
 	fi
 	;;
     "idx" )
 	checksum=`md5 -q $texfile.aux`
-	$LATEXDRAFT $texfile \
-	    && typset_pass=1
+	$LATEXDRAFT $texfile && typset_pass=1
 	if [ $typset_pass = 1 ]; then
 	    message="typeset"
 	    while checksum_before="$checksum" && \
@@ -95,10 +107,11 @@ case "$mode" in
 	    $LATEX $texfile 
 	    message=`echo "$message + MakeIndex + typeset"`
 	fi
+	;;
 esac
 
 if [ $typset_pass = 1 ]; then
-    if [ $pdfdvi = 1 ]; then
+    if [ "$DVIPDF" != "" ]; then
 	$DVIPDF $texfile 
 	message=`echo "$message + DVIPDF"`
     fi
