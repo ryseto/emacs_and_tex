6bc8e322be131bc0f3870dc72addc031b0945741
Ryohei Seto <setoryohei@me.com>
2012-03-23 06:45:25 +0100

    Modified comments.

--- semiautotex.sh
+++ semiautotex.sh
@@ -1,12 +1,19 @@
+##
+##  SemiAutoTeX
+##  
+##
+##  Created by Ryohei SETO on 2012/02/19.
+##  Copyright (c) 2012 Ryohei SETO. All rights reserved.
+##
 ################################################################################
-#### SemiAutoTeX ver 0.04 written by Ryohei Seto
-################################################################################
+#
 #   You can find some explanations at
 #   http://d.hatena.ne.jp/setoryohei/20120219
 #   Many improvements by Kobayashi-san (OSX Workshop)
 #   http://macwiki.sourceforge.jp/wiki/index.php/MacOSX_WorkShop/10.7
 #
 ### Configuration:
+#
 #   You can put an rc file `.texcmdrc' in your home directory.
 #   If there is an rc file `texcmdrc' in current directly, it has the priority.
 #   ホームディレクトリに `.texcmdrc' を置く。
@@ -20,13 +27,16 @@
 #     PDFVIEWER="skim_reload.sh -g"
 #
 ### TeX mode:
+#
 #   aux ファイルがない場合は必ず2回以上実行する。
 #   cross-references を整合させるための typeset 繰り返しは
 #   aux ファイルが変化しなくなるまで続ける。
 #
 ### BibTeX mode:
+#
 #  preprocess:
 #   タイプセット(ドラフトモード)1回は必須。
+#
 #  postprocess:
 #   2回のタイプセット（typeset(d)+typset）は必須。
 #   参考文献リストが書類の途中に置かれている場合は、
@@ -34,27 +44,32 @@
 #   最後のタイプセットは aux ファイルの変化がなくなるまで繰り返す。
 #
 ### MakeIndex mode:
+#
 #  preprocess:
 #   MakeIndex では索引に単語のページ番号を与える必要があるので、
 #   tableofcontents などによりページ番号が変わらなくなるまでタイプセットを繰り返す。
+#
 #  postprocess:
 #   索引作成の影響でページ番号が変わる事はないと想定して後の自動繰り返しはしない。
 #   (もし途中に索引が来てページ番号が変わるなら、MakeIndex から再実行する必要がある。)
 #
 ### Calling DVIPDF:
+#
 #   If DVIPDF is given, i.e. not `nil', 
 #   the DVIPDF program is called after a typesetting process.
+#
 #   変数 DVIPDF が設定されているとき（`nil'以外）は、
 #   タイプセット処理の後にそれを実行し DVI から PDF に変換する。
 #
 ### Calling PDFVIEWER:
+#
 #   If PDFVIEWER is given, i.e. not "nil", 
 #   the previewer proglam is called at the end.
+#
 #   変数 PDFVIEWER が設定されているとき（`nil'以外）は、
 #   全ての処理の後にそれを実行しプレビュアをリロードする。
 #
 ################################################################################
-
 #!/bin/sh
 if [ $# == 0 -o "$1" == "-h" -o "$1" == "-help"  ]; then
     echo "SemiAutoTeX 0.04:  Semi-automatic LaTeX document generation routine
